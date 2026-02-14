20a49a0b9e55ad0e49c3d4f94297bd389080e1ed
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-14 10:06:38 +0530

    set-cookie

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,14 +2,15 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin){
-            callback(null,true);
-        }else{
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
             callback(new Error('Not allowed by cors'))
         }
     },
-    credentials:true,
-    optionsSuccessStatus:200
+    credentials: true,
+    exposedHeaders: ['set-cookie'], // 🔥 REQUIRED
+    optionsSuccessStatus: 200
 }
 
 module.exports = corsOptions;
