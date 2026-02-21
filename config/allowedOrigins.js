78d80ec92f5abbc38f5e06622a598855478c410e
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-21 22:54:29 +0530

    vercel error

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://technotes-pnsi.onrender.com'
+    'https://technotes-pnsi.onrender.com',
+    "https://tech-notes-f7bt1vxzl-mudit-kumar-singhs-projects.vercel.app"
 ];
 
 module.exports = allowedOrigins
